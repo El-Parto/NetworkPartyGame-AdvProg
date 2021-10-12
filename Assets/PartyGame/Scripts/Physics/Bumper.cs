2aebdd43df32fb9052aca9da94f7ccde40148cec
Mesmaa <60990360+Julianm-25@users.noreply.github.com>
2021-10-12 17:32:15 +1100

    Basic Physics and Kicking

---
+++ Assets/PartyGame/Scripts/Physics/Bumper.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace NetworkPartyGame.Physics
+{
+    public class Bumper : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
