4317d2e23dd29b23f6e141e445ee10219c69cf29
xtedx <xtedx@users.noreply.github.com>
2021-10-19 23:15:47 +1100

    ui tabbed menu works, with my toolkit

---
+++ Assets/PartyGame/Scripts/UiManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TeddyToolKit.Core;
+using UnityEngine;
+
+public class UiManager : MonoSingleton<UiManager>
+{
+
+    public void HelloWorld()
+    {
+        Debug.Log("Hello from UI Manager");
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
