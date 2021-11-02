2a524cf7a5d5649ff721b22a567a983d3de53355
Luc <luc980@y7mail.com>
2021-11-02 14:00:46 +1100

    Current version and start on ui stuff

--- Assets/PartyGame/Scripts/Physics/Ball.cs
+++ Assets/PartyGame/Scripts/Physics/Ball.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using UnityEditor.AnimatedValues;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
